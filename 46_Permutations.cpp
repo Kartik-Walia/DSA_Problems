85083c93572550512e511fe3de869e7648dffd34
Kartik-Walia <itzkartikeds3@gmail.com>
2024-01-29 23:31:31 +0530

    Approch II for 46 Permutations (Swapping)

--- 46_Permutations.cpp
+++ 46_Permutations.cpp
@@ -1,34 +1,65 @@
 #include<iostream>
 using namespace std;
 
+
+// ============================================== Approach-I ==============================================
+// class Solution {
+// public:
+//     void Permutations(vector<int> &nums, vector<vector<int>> &ans, vector<int> comp, int map[]) {
+//         // Base Case 
+//         if(comp.size() == nums.size()) {
+//             ans.push_back(comp);
+//             return;
+//         }
+
+//         for(int i=0; i<nums.size(); i++) {
+//             // Checking unmarked (unpicked)
+//             if(!map[i]) {
+//                 comp.push_back(nums[i]);
+//                 map[i] = 1;     // Mark as picked
+//                 Permutations(nums, ans, comp, map);
+//                 comp.pop_back();
+//                 map[i] = 0;     // Mark as unpicked
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> comp;
+//         int map[nums.size()];
+//         for(int i=0; i<nums.size(); i++) map[i] = 0;    // Initialising map with 0
+
+//         Permutations(nums, ans, comp, map);
+
+//         return ans;
+//     }
+// };
+
+
+
+
+// ============================================== Approach-II ==============================================
 class Solution {
 public:
-    void Permutations(vector<int> &nums, vector<vector<int>> &ans, vector<int> comp, int map[]) {
-        // Base Case 
-        if(comp.size() == nums.size()) {
-            ans.push_back(comp);
+    void Permutations(int i, vector<int> &nums, vector<vector<int>> &ans) {
+        // Base case 
+        if(i == nums.size()) {
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=0; i<nums.size(); i++) {
-            // Checking unmarked (unpicked)
-            if(!map[i]) {
-                comp.push_back(nums[i]);
-                map[i] = 1;     // Mark as picked
-                Permutations(nums, ans, comp, map);
-                comp.pop_back();
-                map[i] = 0;     // Mark as unpicked
-            }
+        for(int j=i; j<nums.size(); j++) {
+            swap(nums[i], nums[j]);
+            Permutations(i+1, nums, ans);
+            swap(nums[i], nums[j]);     // Getting back original state by re-swapping
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> comp;
-        int map[nums.size()];
-        for(int i=0; i<nums.size(); i++) map[i] = 0;    // Initialising map with 0
 
-        Permutations(nums, ans, comp, map);
+        Permutations(0, nums, ans);
 
         return ans;
     }
