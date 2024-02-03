1a3502d13df243b9b8421e9269d7fc6af7a9c8e6
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-03 18:30:29 +0530

    70 Climbing Stairs (3 Approaches - Memoization, Tabulation, Space Optimization)

---
+++ 70_ClimbingStairs.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+
+// =============================================== Approach-I (Memoization) ===============================================
+// class Solution {
+// public:
+//     int ways(int n, vector<int> &dp) {
+//         // Base case 
+//         if(n == 0) return 1;
+
+//         // Edge case 
+//         if(n == 1) return 1;
+
+//         int oneStep = (dp[n-1] != -1) ? dp[n-1] : ways(n-1, dp); 
+//         int twoStep = (dp[n-2] != -1) ? dp[n-2] : ways(n-2, dp);
+
+//         return dp[n] = (oneStep + twoStep); 
+//     }
+
+//     int climbStairs(int n) {
+//         vector<int> dp(100,-1);
+//         return ways(n, dp);
+//     }
+// };
+
+
+// =============================================== Approach-II (Tabulation) ===============================================
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(100, -1);
+//         dp[0]=1, dp[1]=1;
+
+//         // Base case 
+//         if(n == 0) return dp[0];
+//         if(n == 1) return dp[1];
+        
+//         for(int i=2; i<=n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//             if(i==n) return dp[i];
+//         }
+
+//         return 0;
+//     }
+// };
+
+
+// ============================================ Approach-III (Space Optimization) ============================================
+class Solution {
+public:
+    int climbStairs(int n) {
+        int prev2=0, prev=1;
+        for(int i=2; i<=n; i++) {
+            int curi = prev + prev2;
+            prev2 = prev;
+            prev = curi;
+        }
+        // At end of loop, prev and prev2 store the counts for the two previous steps n-1 and n-2
+        return prev + prev2;    // Their sum gives total number of ways to reach the top
+    }
+};
