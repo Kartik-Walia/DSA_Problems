d1a45bb82bfdbc19b8fc736c6c42ea9ba4487027
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-06 18:13:11 +0530

    2487 Remove Nodes from Linked List

--- Leetcode/2487_RemoveNodesFromLinkedList.cpp
+++ Leetcode/2487_RemoveNodesFromLinkedList.cpp
@@ -9,43 +9,53 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
-public:
-    ListNode* reverseLL(ListNode* head) {
-        ListNode* prev = NULL;
-        ListNode* curr = head;
 
-        while(curr != NULL) {
-            ListNode* nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
+// ================================================ Approach-I ================================================
+// class Solution {
+// public:
+//     ListNode* reverseLL(ListNode* head) {
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
 
-        return prev;
-    }
+//         while(curr != NULL) {
+//             ListNode* nextNode = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = nextNode;
+//         }
 
-    ListNode* removeNodes(ListNode* head) {
-        head = reverseLL(head);
+//         return prev;
+//     }
 
-        int maxVal = head->val;
-        ListNode* prev = head;
-        ListNode* curr = head->next;
-        while(curr != NULL) {
-            int value = curr->val;
-            maxVal = max(maxVal, value);
-            if(value < maxVal) {
-                // Delete node with value = val
-                prev->next = curr->next;
-                curr->next = NULL;
-                delete curr;
-                curr = prev->next;
-            } else {
-                prev = curr;
-                curr = curr->next;
-            }
-        }
+//     ListNode* removeNodes(ListNode* head) {
+//         head = reverseLL(head);
 
-        return reverseLL(head);
-    }
-};
+//         int maxVal = head->val;
+//         ListNode* prev = head;
+//         ListNode* curr = head->next;
+//         while(curr != NULL) {
+//             int value = curr->val;
+//             maxVal = max(maxVal, value);
+//             if(value < maxVal) {
+//                 // Delete node with value = val
+//                 prev->next = curr->next;
+//                 curr->next = NULL;
+//                 delete curr;
+//                 curr = prev->next;
+//             } else {
+//                 prev = curr;
+//                 curr = curr->next;
+//             }
+//         }
+
+//         return reverseLL(head);
+//     }
+// };
+
+
+// ================================================ Approach-II ================================================
+ListNode* removeNodes(ListNode* head) {
+    if (!head) return NULL;
+    head->next = removeNodes(head->next);
+    return head->next && head->val < head->next->val ?  head->next : head;
+}
