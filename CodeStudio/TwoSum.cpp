e23806d5136cfe368ecc2d5fb9faf9bbc99604a5
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-08 22:08:24 +0530

    Added TC &

--- CodeStudio/TwoSum.cpp
+++ CodeStudio/TwoSum.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string read(int n, vector<int> book, int target) {
     int L=0, R=n-1;    // 2 pointers
 
-    sort(book.begin(), book.end());
+    sort(book.begin(), book.end());     // O(NlogN)
 
     while(L <= R) {
         if(book[L] + book[R] > target) R--;
@@ -15,3 +15,6 @@ string read(int n, vector<int> book, int target) {
     
     return "NO";
 }
+
+// TC : O(NlogN) + O(N)
+// SC : O(1)    [not using any extra space]     [If you want to take in account the SC array we sorted, then it's going to be O(N)]
