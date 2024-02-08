e8b331ac4a4b58ca71e31f7c80bb975b66113900
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-08 12:59:31 +0530

    Merge 2 sorted Arrays (CodeStudio) (Brute - Better - Optimal)

---
+++ CodeStudio/Merge2SortedArray.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+using namespace std;
+
+
+// ======================================================= Approach-I (Brute Force) =======================================================
+#include<set>
+vector<int> sortedArray(vector<int> a, vector<int> b) {
+    set<int> st;    // Set stores unique values only and that too in ascending order
+    vector<int> ans;
+
+    for(int i:a) st.insert(i);
+    for(int i:b) st.insert(i);
+
+    for(auto it:st) ans.push_back(it);
+
+    return ans;
+}
+// TC : O(nlogn) + O(mlogm) + O(n+m)
+// SC : O(n+m) + O(n+m)    [1st is for Set & 2nd is used to return the ans, not using 2nd to solve the problem]
+
+
+// ==================================================== Approach-II (Better Approach) ====================================================
+vector<int> sortedArray(vector<int> a, vector<int> b) {
+    int i=0, j=0;   // 2 pointers
+    int last=-1;   // To store last value pushed in ans for avoiding duplicates
+    vector<int> ans;
+    
+    while(i<a.size() && j<b.size()) {
+        if(a[i] == b[j]) {
+            if(a[i] != last) {
+                ans.push_back(a[i]);
+                last = a[i];
+            }
+            i++;
+            j++;
+        }
+        else if(a[i] > b[j]) {
+            if(b[j] != last) {
+                ans.push_back(b[j]);
+                last = b[j];
+            }
+            j++;
+        }
+        else if(a[i] < b[j]) {
+            if(a[i] != last) {
+                ans.push_back(a[i]);
+                last = a[i];
+            }
+            i++;
+        }
+    }
+
+    // 2nd array got exhausted
+    while(i<a.size()) {
+        if (a[i] != last) {
+            last = a[i];
+            ans.push_back(a[i]);
+        }
+        i++;
+    }
+
+    // 1st array got exhausted
+    while(j<b.size()) {
+        if (b[j] != last) {
+            last = b[j];
+            ans.push_back(b[j++]);
+        }
+        j++;
+    }
+
+    return ans;
+}
+
+
+// ==================================================== Approach-III (Optimal Approach) ====================================================
+vector<int> sortedArray(vector<int> a, vector<int> b) {
+    int i=0, j=0;   // 2 pointers
+    int last=-1;   // To store last value pushed in ans for avoiding duplicates
+    vector<int> ans;
+    
+    while(i<a.size() && j<b.size()) {
+        if(a[i] > b[j]) {
+            if(b[j] != last) {
+                ans.push_back(b[j]);
+                last = b[j];
+            }
+            j++;
+        }
+        else {
+            if(a[i] != last) {
+                ans.push_back(a[i]);
+                last = a[i];
+            }
+            i++;
+            if(a[i] == b[j]) j++;
+        }
+    }
+
+    // 2nd array got exhausted
+    while(i<a.size()) {
+        if (a[i] != last) {
+            last = a[i];
+            ans.push_back(a[i]);
+        }
+        i++;
+    }
+
+    // 1st array got exhausted
+    while(j<b.size()) {
+        if (b[j] != last) {
+            last = b[j];
+            ans.push_back(b[j++]);
+        }
+        j++;
+    }
+
+    return ans;
+}
