069fd3cbbc4fc0e4ecea1b7450d0786356913f0a
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-08 13:13:43 +0530

    Merge 2 sorted arrays (CodeStudio) (Brute - Optimal) (Added TC & SC)

--- CodeStudio/Merge2SortedArray.cpp
+++ CodeStudio/Merge2SortedArray.cpp
@@ -19,60 +19,7 @@ vector<int> sortedArray(vector<int> a, vector<int> b) {
 // SC : O(n+m) + O(n+m)    [1st is for Set & 2nd is used to return the ans, not using 2nd to solve the problem]
 
 
-// ==================================================== Approach-II (Better Approach) ====================================================
-vector<int> sortedArray(vector<int> a, vector<int> b) {
-    int i=0, j=0;   // 2 pointers
-    int last=-1;   // To store last value pushed in ans for avoiding duplicates
-    vector<int> ans;
-    
-    while(i<a.size() && j<b.size()) {
-        if(a[i] == b[j]) {
-            if(a[i] != last) {
-                ans.push_back(a[i]);
-                last = a[i];
-            }
-            i++;
-            j++;
-        }
-        else if(a[i] > b[j]) {
-            if(b[j] != last) {
-                ans.push_back(b[j]);
-                last = b[j];
-            }
-            j++;
-        }
-        else if(a[i] < b[j]) {
-            if(a[i] != last) {
-                ans.push_back(a[i]);
-                last = a[i];
-            }
-            i++;
-        }
-    }
-
-    // 2nd array got exhausted
-    while(i<a.size()) {
-        if (a[i] != last) {
-            last = a[i];
-            ans.push_back(a[i]);
-        }
-        i++;
-    }
-
-    // 1st array got exhausted
-    while(j<b.size()) {
-        if (b[j] != last) {
-            last = b[j];
-            ans.push_back(b[j++]);
-        }
-        j++;
-    }
-
-    return ans;
-}
-
-
-// ==================================================== Approach-III (Optimal Approach) ====================================================
+// ==================================================== Approach-II (Optimal Approach) ====================================================
 vector<int> sortedArray(vector<int> a, vector<int> b) {
     int i=0, j=0;   // 2 pointers
     int last=-1;   // To store last value pushed in ans for avoiding duplicates
@@ -116,3 +63,5 @@ vector<int> sortedArray(vector<int> a, vector<int> b) {
 
     return ans;
 }
+// TC : O(n+m)
+// SC : O(n+m) for returning ans, not for solving [For algorithm it's O(1)]
