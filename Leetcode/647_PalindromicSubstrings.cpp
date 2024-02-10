33d3b8e44573bfa953267ba4e78c5bda67a0cdbe
Kartik-Walia <itzkartikeds3@gmail.com>
2024-02-10 18:30:22 +0530

    647 Palindromic Substrings (Leetcode) (Brute)

---
+++ Leetcode/647_PalindromicSubstrings.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+// ======================================================= Approach-I (Brute Force) =======================================================
+class Solution {
+public:
+    bool isPalindrome(int i, int j, string &s) {
+        while(i <= j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    } 
+
+    int countSubstrings(string s) {
+        int count=0;
+
+        for(int i=0; i<s.size(); i++) {
+            for(int j=i; j<s.size(); j++) {
+                if(isPalindrome(i,j,s)) count++;
+            }
+        }
+
+        return count;
+    }
+};
