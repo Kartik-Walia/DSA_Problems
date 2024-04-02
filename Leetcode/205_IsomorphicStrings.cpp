f5d0a6f5e90e0ea41f5c573fce7c52c7671ca9fd
Kartik-Walia <itzkartikeds3@gmail.com>
2024-04-03 01:23:29 +0530

    205 Isomorphic Strings (LeetCode)

---
+++ Leetcode/205_IsomorphicStrings.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> mpST, mpTS;
+        for(int i=0; i<s.length(); i++){
+            if(mpST[s[i]] && mpST[s[i]]!=t[i]) return false;
+            if(mpTS[t[i]] && mpTS[t[i]]!=s[i]) return false;
+            mpST[s[i]] = t[i];
+            mpTS[t[i]] = s[i];
+        }
+        return true;
+    }
+};
